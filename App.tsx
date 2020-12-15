5fec1edb83391bd9f9ce36419caab7dee5190674
yaelleC <yaelle.chaudy@gmail.com>
2020-12-15 16:41:57 +0100

    Added ReaderView component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+import ReaderView from "./components/ReaderView";
+
+const logo = require("./images/ramuh.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Racconte-moi une histoire!</Text>
+      <Image style={styles.tinyLogo} source={logo} />
+      <Text>Welcome!</Text>
       <StatusBar style="auto" />
+      <ReaderView />
     </View>
   );
 }
@@ -14,8 +20,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  tinyLogo: {
+    width: 500,
+    height: 500,
   },
 });
